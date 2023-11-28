dece7778fd6958528b489c0d686b8d9d8435d071
NathanMacktravis <95182428+NathanMacktravis@users.noreply.github.com>
2023-11-28 15:32:52 +0100

    Update create_API.py

--- create_API.py
+++ create_API.py
@@ -1,12 +1,12 @@
 import random
 from fastapi import FastAPI
-
+from create_API import movies
 # Création de l'application fastAPI
 app = FastAPI()
-
+movies_titles = movies()
 
 # Créer le premier edpoint
 @app.get("/")
 async def root():
-    return {"Messsage" : "Bienvenue sur notre API de génération de nombre aléatoires"}
+    return movies_titles
 
